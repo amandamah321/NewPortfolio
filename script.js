ae2e7363ee95024a483eb2d7f25fd474490ef476
amandamah321 <amanda_mah321@hotmail.com>
2024-05-15 17:13:01 +0200

    subTitle script add

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function removeAction() {
 }
 
 /**Typying effect**/
-
+/*
 function typingLetter(element) {
   const arrText = element.innerHTML.split("");
   element.innerHTML = "";
@@ -30,7 +30,7 @@ function typingLetter(element) {
 
 const title = document.querySelector(".typing");
 
-typingLetter(title);
+typingLetter(title);*/
 
 /****Hamburger Menu******/
 
@@ -57,16 +57,139 @@ menu_ham.addEventListener("click", () => {
 
 /*****Animation Aside***/
 
-const sections = document.querySelectorAll('.animationAside');
+const sections = document.querySelectorAll(".animationAside");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 700;
     let height = sec.offsetHeight;
 
     if (top >= offset && top < offset + height) {
-      sec.classList.add('show_animation');
+      sec.classList.add("show_animation");
     }
-  })
+  });
+};
+
+/****Projects Display***/
+
+var data = [
+  {
+    title: "Quiz",
+    textDesc: "This project was crafted utilizing JavaScript, users are able to check their knoledge about the environment.",
+    skill: ["html", "css", "js"],
+    imageUrl: "quiz",
+  },
+  {
+    title: "Real State Agency Website",
+    textDesc: "This project was crafted as an Angular project, designed to enhance proficiency in using Angular components",
+    skill: ["angular", "js"],
+    imageUrl: "realstate",
+  },
+  
+  {
+    title: "E-players Website",
+    textDesc: "This project was built using figma and javaScript.",
+    skill: ["html", "css", "js"],
+    imageUrl: "eplayers",
+  },  
+  {
+    title: "React Simple Calculador",
+    textDesc: "This project was built utilizing React components, designed specifically to improve proficiency in React functions",
+    skill: ["css", "react", "typescript"],
+    imageUrl: "calculador",
+  },
+
+  {
+    title: "English Dictionary",
+    textDesc:
+      "This is a project meticulously crafted with JavaScript, leveraging an API to fetch data and seamlessly deliver it to the user.",
+    skill: ["html", "css", "js"],
+    imageUrl: "dictionary",
+  },
+
+  {
+    title: "ClothesMe.com Website",
+    textDesc: "An Ecommerce project made by using Html and its validations",
+    skill: ["html", "css", "js"],
+    imageUrl: "ecommerce",
+  },
+  
+  
+];
+
+function displayProjects() {
+  data.map((element) => {
+    let containerEl = document.querySelector(".project-section");
+
+    let divProject = document.createElement("div");
+
+    divProject.innerHTML = `
+    
+            <div class="project-container">
+                <img class="project-image" src="./assets/projects/${element.imageUrl}.png" alt="">
+
+                <div class="project-description">
+
+                    <h2>${element.title}</h2>
+                    <p>${element.textDesc}</p>
+                    
+                    <div class="skills">
+                        <img class="skill" src="./assets/languagesImg/${element.skill[0]}.png" alt="">
+                        <img class="skill" src="./assets/languagesImg/${element.skill[1]}.png" alt="">
+                        <img class="skill" src="./assets/languagesImg/${element.skill[2]}.png" alt="">
+                    </div>
+                    
+                </div>
+
+            </div>
+    `;
+
+    containerEl.appendChild(divProject);
+  });
+}
+
+displayProjects();
+
+
+/*******************Typing and Erasing Words*****************/
+
+const typeText = document.querySelector(".typed-text");
+const words = ["Front-end Developer", "Back-end Developer", "Full-stack Developer", "Software Engineer"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newLetterDelay = 300;
+let index = 0;
+let charIndex = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  if (words.length) {
+    setTimeout(type, newLetterDelay);
+  }
+});
+
+function type() {
+  if (charIndex < words[index].length) {
+    typeText.textContent += words[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newLetterDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typeText.textContent = words[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    index++;
+    if (index >= words.length) {
+      index = 0;
+    }
+
+    setTimeout(type, typingDelay + 100);
+  }
 }
